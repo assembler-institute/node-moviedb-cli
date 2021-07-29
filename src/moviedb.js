ccd1d02e4db4d1451e1f70c1620129be9a3ca00b
Ricard Garcia <imricardgarcia@gmail.com>
2021-07-29 19:42:38 +0200

    feat: added initial options and dotenv functionality

--- src/moviedb.js
+++ src/moviedb.js
@@ -1,15 +1,29 @@
 #!/usr/bin/env node
-
 const { Command } = require("commander");
-
+const dotenv = require("dotenv");
 const program = new Command();
+
+dotenv.config();
+
+console.log("This is the API key: ", process.env.API_KEY);
 program.version("0.0.1");
 
+// Options ---------------------------------------------------
+program
+  // Get persons
+  .requiredOption("-p, --popular", "Fetch the popular persons")
+  .requiredOption("--page", "The page of persons data results to fetch");
+
+const opt = program.opts();
+// debugger;
+console.log(opt);
+
+// Commands ---------------------------------------------------
 program
   .command("get-persons")
   .description("Make a network request to fetch most popular persons")
   .action(function handleAction() {
-    console.log("hello-world");
+    console.log("Get persons at page: ", opt.page);
   });
 
 program
