24595228cd438ac3dfc349de8cb93e74745c339d
hugo duran <ls>
2021-07-31 11:56:43 +0200

    -

--- src/moviedb.js
+++ src/moviedb.js
@@ -10,15 +10,23 @@ program.version("0.0.1");
 program
   .command("get-persons")
   .description("Make a network request to fetch most popular persons")
-
-  .action(function handleAction() {});
+  .requiredOption(
+    "--page <number>",
+    "The page of persons data results to fetch"
+  );
+option("-p, --popular", "Fetch the popular persons");
+action(async function handleAction(options) {
+  const page = parseInt(options.page);
+  const json = await request.getPopularPersons(page);
+  console.log(json);
+});
 
 program
   .command("get-person")
   .description("Make a network request to fetch the data of a single person");
 requiredOption("-i, --id <number> ", "The id of the person");
 action(async function handleAction(options) {
-  const personId = parseInt(options.id, 10);
+  const personId = parseInt(options.id);
   const json = await request.getPerson(personId);
   console.log(json);
 });
