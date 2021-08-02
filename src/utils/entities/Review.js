0776aea5482c7716d3af9c933d3812d2f9cc8745
Brahim <brahimcasas@hotmail.es>
2021-08-02 14:43:37 +0200

    feat: single movie review

--- src/utils/entities/Review.js
+++ src/utils/entities/Review.js
@@ -4,6 +4,8 @@ const log = console.log;
 class Review {
   constructor(movie) {
     this.id = movie.id;
+    this.page = movie.page;
+    this.total_pages = movie.total_pages;
     this.reviews = movie.results;
   }
 
@@ -11,12 +13,15 @@ class Review {
     log(chalk.white("\n_________________________"));
     log("\n");
 
-    if (this.reviews.length)
+    if (this.reviews.length) {
       this.reviews.map((review) => {
         log(`Author: ${chalk.blue.bold(review.author)}`);
         log(`content: ${chalk.white(this.sliceString(review.content))}`);
       });
-    else log(`The movie: ${this.id} doesn’t have any reviews`);
+
+      log(chalk.white("________________________________________________"));
+      log(chalk.white(`Page: ${this.page} of: ${this.total_pages}`));
+    } else log(`The movie: ${this.id} doesn’t have any reviews`);
 
     log("\n");
   }
