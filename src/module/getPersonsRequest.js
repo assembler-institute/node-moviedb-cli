daa724def80c2f92a67d6cea53104a9779c81015
Jonathan Cedeño <jony.cede@gmail.com>
2021-08-02 08:39:14 +0200

    updated getPersons function

--- src/module/getPersonsRequest.js
+++ src/module/getPersonsRequest.js
@@ -1,14 +1,18 @@
 const https = require("https");
 const chalk = require("chalk");
-require("dotenv").config();
+
+const {
+  API_KEY,
+  API_HOSTNAME,
+  API_TIMEOUT_REQUEST,
+  SECTION_SEPARATOR,
+} = require("../utils/constants");
 
 const spinner = require("../components/spinner");
-const API_KEY = process.env.API_KEY;
-const separator = "--------------------------------------";
 
 function getPersons({ page }) {
   const options = {
-    hostname: "api.themoviedb.org",
+    hostname: API_HOSTNAME,
     port: 443,
     path: `/3/person/popular?page=${page}&api_key=${API_KEY}`,
     method: "GET",
@@ -20,8 +24,7 @@ function getPersons({ page }) {
     res.on("data", (d) => {
       body += d;
 
-      spinner.color = "red";
-      spinner.start("Loading data...");
+      spinner.start("Fetching the popular person's data...\n\n");
     });
 
     res.on("end", () => {
@@ -31,64 +34,71 @@ function getPersons({ page }) {
 
         if (dataObject.errors) {
           spinner.fail(dataObject.errors[0]);
-        }
+        } else {
+          const results = dataObject.results;
+          results.forEach((r) => {
+            // Separator
+            console.log(chalk.white(`${SECTION_SEPARATOR}\n`));
 
-        const results = dataObject.results;
-        results.forEach((r) => {
-          // Title person
-          console.log(chalk.white("Person:\n"));
+            // Title person
+            console.log(chalk.white("Person:\n"));
 
-          // ID property
-          console.log(chalk.white(`ID: ${r.id}\t`));
+            // ID property
+            console.log(chalk.white(`ID: ${r.id}`));
 
-          // Name property
-          console.log(`Name: ${chalk.bold.blue(r.name)}`);
+            // Name property
+            console.log(`Name: ${chalk.bold.blue(r.name)}`);
 
-          // known_for_department property
-          if (r.known_for_department == "Acting") {
-            console.log(
-              `Department: ${chalk.magenta(r.known_for_department)}\n`
-            );
-          }
+            // known_for_department property
+            if (r.known_for_department == "Acting") {
+              console.log(
+                `Department: ${chalk.magenta(r.known_for_department)}`
+              );
+            }
 
-          // Appearing in movies section
-          let cnt = 0;
-          r.known_for.forEach((movie) => {
-            if (movie.title) {
-              if (cnt == 0) {
-                console.log("Appearing in movies:\n");
+            // Empty line
+            console.log("");
+
+            // Appearing in movies section
+            let cnt = 0;
+            r.known_for.forEach((movie) => {
+              if (movie.title) {
+                if (cnt == 0) {
+                  console.log(chalk.white("Appearing in movies:\n"));
+                }
+                console.log(chalk.white(`\tMovie:`));
+                console.log(chalk.white(`\tID: ${movie.id}`));
+                console.log(
+                  chalk.white(`\tRelease date: ${movie.release_date}`)
+                );
+                console.log(chalk.white(`\tTitle: ${movie.title}\n`));
+                cnt += 1;
               }
-              console.log(chalk.white(`\tMovie:\t`));
-              console.log(chalk.white(`\tID: ${movie.id}`));
-              console.log(chalk.white(`\tRelease date: ${movie.release_date}`));
-              console.log(chalk.white(`\tTitle: ${movie.title}\n`));
-              cnt += 1;
+            });
+            if (cnt == 0) {
+              console.log(
+                chalk.hex("#FFA500")(`${r.name} doesn’t appear in any movie\n`)
+              );
             }
           });
-          if (cnt == 0) {
+
+          // Page index
+          if (page <= dataObject.total_pages) {
+            console.log(chalk.white(SECTION_SEPARATOR));
             console.log(
-              chalk.hex("#FFA500")(`${r.name} doesn’t appear in any movie\n`)
+              chalk.white(`Page ${page} of ${dataObject.total_pages}\n\n\n`)
             );
           }
 
-          // Separator for each person
-          console.log(`${separator}\n`);
-        });
-
-        if (page <= dataObject.total_pages) {
-          console.log(
-            chalk.white(`Page ${page} of ${dataObject.total_pages}\n`)
-          );
+          spinner.stop();
+          spinner.succeed("Popular Persons data loaded");
         }
-
-        spinner.stop();
-        spinner.succeed("Popular Persons data loaded");
-      }, 1000);
+      }, API_TIMEOUT_REQUEST);
     });
   });
 
   req.on("error", (e) => {
-    spinner.fail(`Error number:${e.errno} => code:${e.code}`);
+    spinner.fail(`Error "${e.code}" (Code ${e.errno})`);
   });
 
   req.end();
