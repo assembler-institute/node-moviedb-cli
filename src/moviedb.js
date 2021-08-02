f32ed10f4d85e75aeb3b28ea585775170b1b26cd
Jordi <aejordi@gmail.com>
2021-08-02 17:54:37 +0200

    enh: updated main with some changes

--- src/moviedb.js
+++ src/moviedb.js
@@ -3,7 +3,6 @@
 const { Command } = require("commander");
 const req = require("./utils/httprequest");
 const date = require("./utils/httprequest");
-const chalk = require('chalk');
 require("dotenv/config");
 
 const program = new Command();
@@ -38,35 +37,17 @@ program
 program
   .command("get-movie")
   .description("Make a network request to fetch the data of a single person")
-  .requiredOption('-i, --id <id> ','The id of the movie')
-  .option('-r, --review','Fetch the reviews of the movie')
+  .requiredOption("-i, --id <id> ", "The id of the movie")
+  .option("-r, --review", "Fetch the reviews of the movie")
 
   .action(function handleAction(options) {
     let movieId = options.id;
     let movieReview = options.review;
-    // console.log(JSON.stringify(options));
-    // console.log(movieId);
-    // console.log(movieReview);
-    // console.dir(options);
-    if(movieReview) {
-      console.log('Entra a la review')
-       req.httpRequest("movie/" + movieId + "/reviews");
-  }else{
-      console.log('Entra con el ID');
+    if (movieReview) {
+      req.httpRequest("movie/" + movieId + "/reviews");
+    } else {
       req.httpRequest("movie/" + movieId);
-  }
-
-
-
-  });
-
-
-
-  program
-  .command("get-api-key") 
-  .description("Make a network request to fetch the data of a single person")
-  .action(function handleAction() {
-    // console.log(process.env.API_KEY);
+    }
   });
 
 // error on unknown commands
--- src/utils/httprequest.js
+++ src/utils/httprequest.js
@@ -1,46 +1,43 @@
 const https = require("https");
 const fs = require("fs");
 const ora = require("ora");
-const chalk = require('chalk');
+const chalk = require("chalk");
 const { movie_render } = require("./movie_render");
 const spinner = require("./spinner_persons");
 const render = require("./render_persons");
 require("dotenv/config");
 
 exports.httpRequest = function (endPoint, option1 = "", option2 = "") {
-  const spinner = ora('Loading Film').start();
-  https.get(
-    `https://api.themoviedb.org/3/${endPoint}?api_key=${process.env.API_KEY}&${option1}&${option2}`,
-    (response) => {
-        let result = '';
+  const spinner = ora("Loading Film").start();
+  https
+    .get(
+      `https://api.themoviedb.org/3/${endPoint}?api_key=${process.env.API_KEY}&${option1}&${option2}`,
+      (response) => {
+        let result = "";
         response.on("data", (d) => {
-        result += d;
-      });
+          result += d;
+        });
 
-      response.on("end", () => {
-        if (result === "") {
-          spin.fail("Error: can't find your request");
-          return;
-        }
+        response.on("end", () => {
+          if (result === "") {
+            spin.fail("Error: can't find your request");
+            return;
+          }
 
-        switch (endPoint) {
-          case "person/popular":
-            render.persons(JSON.parse(result));
-            break;
-          case 'movie/70':
-            let obj = JSON.parse(result);
-            movie_render(obj);
-            spinner.succeed('Film loaded successfully');
-            break;
-        }
-      
-      });
-
-    }
-  ).on ('error', (err) => {
-    spinner.fail('Error: ' + err.message);
-  });
+          switch (endPoint) {
+            case "person/popular":
+              render.persons(JSON.parse(result));
+              break;
+            case "movie/70":
+              movie_render(JSON.parse(result));
+              spinner.succeed("Film loaded successfully");
+              break;
+          }
+        });
+      }
+    )
+    .on("error", (err) => {
+      spinner.fail("Error: " + err.message);
+    });
   spinner.stop();
-}
-
-
+};
