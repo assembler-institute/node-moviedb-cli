c592929b5da36aad150065565e82293f5e29c315
sergi92 <45389112+sergi92@users.noreply.github.com>
2021-12-10 14:17:20 +0100

    api sin error

--- api.js
+++ api.js
@@ -1,24 +1,9 @@
 const https = require('https');
 require('dotenv').config();
-const BASE_URL = "https://api.themoviedb.org/3";
-const popular = "https://api.themoviedb.org/3/person/popular?page=1";
-const PORT = process.env.PORT || 3000;
-var express = require('express');
-var fs = require('fs');
-var app = express();
-
 const apiKey = process.env.API_KEY;
 
-var options = {
-    host: 'api.themoviedb.org',
-    path: '/3/person/popular?page=1',
-    method: 'GET',
-    ca: apiKey
 
-};
-console.log(apiKey);
-
-https.request(options, (res) => {
+https.request(`https://api.themoviedb.org/3/movie/76341?api_key=${apiKey}`, (res) => {
 
     console.log(`statusCode: ${res.statusCode}`);
 
