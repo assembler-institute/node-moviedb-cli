44ac338e855478abd1bec85bed5496a166ca0e6f
Daniella Alamo <dalamov08@gmail.com>
2021-12-10 12:03:14 +0100

    .envExample

---
+++ src/env.js
@@ -0,0 +1,3 @@
+require('dotenv').config();
+const http = require("http");
+const KEY = process.env.API_KEY;
--- src/moviedb.js
+++ src/moviedb.js
@@ -8,21 +8,21 @@ program.version("0.0.1");
 program
   .command("get-persons")
   .description("Make a network request to fetch most popular persons")
-  .action(function handleAction() {
-    console.log("hello-world");
+  .action(function getPersons() {
+    // code here
   });
 
 program
   .command("get-person")
   .description("Make a network request to fetch the data of a single person")
-  .action(function handleAction() {
-    console.log("hello-world");
+  .action(function getPerson() {
+    // code here
   });
 
 program
   .command("get-movies")
   .description("Make a network request to fetch movies")
-  .action(function handleAction() {
+  .action(function getMovies() {
     console.log("hello-world");
   });
 
