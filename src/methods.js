5d382620ecc08f3cc8db513c7c70086cd01fda1d
andarbech <andarbech1.com>
2021-12-13 11:15:30 +0100

    functional json

--- src/methods.js
+++ src/methods.js
@@ -3,24 +3,29 @@ require('dotenv').config();
 const ora = require('ora');
 const https = require('https');
 const chalk = require("chalk");
-const { renderPersonsData } = require("./outputsMetods.js");
+// const { renderPersonsData } = require("./outputsMetods.js");
+
 //------------------------------------#########-----------------------------------------------------
+const log = console.log;
 
 const apiKey = process.env.API_KEY;
 
 
 function connectApi(page) {
-    const url = `https://api.themoviedb.org/3/person/popular?api_key=${apiKey}&language=en-US&page=${page}`
+    const url = `https://api.themoviedb.org/3/person/popular?api_key=${apiKey}&page=${page}`;
     const req = https.request(url, (res) => {
+        let data = "";
         res.setEncoding('utf8');
-        res.on("data", (data) => {
-            // ora(' Fetching the popular person\'s data...').start()
-            renderPersonsData(data)
-                // ora().succeed("finish");
-                // ora().stopAndPersist();
+        res.on("data", (d) => {
+            data += d;
         });
-    });
+        res.on("end", () => {
+            let obj = JSON.parse(data);
+            log(obj);
+        })
+    })
     req.end()
+
 }
 module.exports = {
     connectApi,
