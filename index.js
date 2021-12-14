f03c27b05f92127c54d627bb6c55fdcd68a13cf7
ParisArcos <84467828+ParisArcos@users.noreply.github.com>
2021-12-14 09:07:04 +0000

    index -> deleted

--- index.js
+++
@@ -1,5 +0,0 @@
-//? para poder acceder al contenido del .env
-require("dotenv").config();
-const { program } = require("commander");
-program.version("0.0.1");
-// TODO API-key === process.env.API_KEY
